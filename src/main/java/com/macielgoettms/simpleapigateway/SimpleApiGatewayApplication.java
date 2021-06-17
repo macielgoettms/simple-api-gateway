abec112f4841158d62a5eda0e36ac9986bd0d960
Maciel Goettms <maciel@microsys.inf.br>
2021-06-16 21:22:38 -0300

    add project

---
+++ src/main/java/com/macielgoettms/simpleapigateway/SimpleApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.macielgoettms.simpleapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/macielgoettms/simpleapigateway/SimpleApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macielgoettms.simpleapigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
